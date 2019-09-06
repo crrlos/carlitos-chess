04d532075d21b27a814d1540efdbf64c38b9a942
Carlos Ramírez <rlobos.carlos@gmail.com>
2019-09-05 22:02:12 -0600

    Implementacion movimientos de rey

--- src/app/Piezas/Rey.java
+++ src/app/Piezas/Rey.java
@@ -1,5 +1,6 @@
 package app.Piezas;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -12,7 +13,72 @@ public class Rey implements Pieza{
     }
     @Override
     public List<int[]> ObtenerMovimientos(Pieza[][] tablero, int[] posicion) {
-        return null;
+        int filaOrigen = posicion[0];
+        int columnaOrigen = posicion[1];
+
+        var lista = new ArrayList<int[]>();
+
+        if(filaOrigen + 1 < 8){
+
+            if(tablero[filaOrigen + 1][columnaOrigen] == null)
+                lista.add( new int[]{filaOrigen,columnaOrigen,filaOrigen + 1,columnaOrigen});
+            else
+                if(tablero[filaOrigen + 1][columnaOrigen].EsBlanca() != this.EsBlanca())
+                    lista.add( new int[]{filaOrigen,columnaOrigen,filaOrigen + 1,columnaOrigen});
+            if(columnaOrigen + 1 < 8)        
+            if(tablero[filaOrigen + 1][columnaOrigen + 1] == null)
+                lista.add( new int[]{filaOrigen,columnaOrigen,filaOrigen + 1,columnaOrigen + 1});
+            else
+                if(tablero[filaOrigen + 1][columnaOrigen + 1 ].EsBlanca() != this.EsBlanca())
+                    lista.add( new int[]{filaOrigen,columnaOrigen,filaOrigen + 1,columnaOrigen +1});
+
+            if(columnaOrigen - 1 >= 0)        
+            if(tablero[filaOrigen + 1][columnaOrigen - 1] == null)
+                lista.add( new int[]{filaOrigen,columnaOrigen,filaOrigen + 1,columnaOrigen - 1});
+            else
+                if(tablero[filaOrigen + 1][columnaOrigen - 1 ].EsBlanca() != this.EsBlanca())
+                    lista.add( new int[]{filaOrigen,columnaOrigen,filaOrigen + 1,columnaOrigen -1});
+        }
+        if(filaOrigen - 1 >= 0){
+
+            if(tablero[filaOrigen - 1][columnaOrigen] == null)
+                lista.add( new int[]{filaOrigen,columnaOrigen,filaOrigen - 1,columnaOrigen});
+            else
+                if(tablero[filaOrigen - 1][columnaOrigen].EsBlanca() != this.EsBlanca())
+                    lista.add( new int[]{filaOrigen,columnaOrigen,filaOrigen - 1,columnaOrigen});
+            if(columnaOrigen + 1 < 8)        
+            if(tablero[filaOrigen - 1][columnaOrigen + 1] == null)
+                lista.add( new int[]{filaOrigen,columnaOrigen,filaOrigen - 1,columnaOrigen + 1});
+            else
+                if(tablero[filaOrigen - 1][columnaOrigen + 1 ].EsBlanca() != this.EsBlanca())
+                    lista.add( new int[]{filaOrigen,columnaOrigen,filaOrigen - 1,columnaOrigen +1});
+
+            if(columnaOrigen - 1 >= 0)        
+            if(tablero[filaOrigen - 1][columnaOrigen - 1] == null)
+                lista.add( new int[]{filaOrigen,columnaOrigen,filaOrigen - 1,columnaOrigen - 1});
+            else
+                if(tablero[filaOrigen - 1][columnaOrigen - 1 ].EsBlanca() != this.EsBlanca())
+                    lista.add( new int[]{filaOrigen,columnaOrigen,filaOrigen - 1,columnaOrigen -1});
+            
+        }
+
+        if(columnaOrigen + 1 < 8){
+            if(tablero[filaOrigen ][columnaOrigen + 1] == null)
+                lista.add( new int[]{filaOrigen,columnaOrigen,filaOrigen,columnaOrigen + 1});
+            else
+                if(tablero[filaOrigen][columnaOrigen + 1 ].EsBlanca() != this.EsBlanca())
+                    lista.add( new int[]{filaOrigen,columnaOrigen,filaOrigen,columnaOrigen +1});
+        }
+        if(columnaOrigen - 1 >= 0){
+            if(tablero[filaOrigen ][columnaOrigen - 1] == null)
+                lista.add( new int[]{filaOrigen,columnaOrigen,filaOrigen,columnaOrigen - 1});
+            else
+                if(tablero[filaOrigen][columnaOrigen -1 ].EsBlanca() != this.EsBlanca())
+                    lista.add( new int[]{filaOrigen,columnaOrigen,filaOrigen,columnaOrigen -1});
+        }
+
+
+        return lista;
     }
 
     @Override
