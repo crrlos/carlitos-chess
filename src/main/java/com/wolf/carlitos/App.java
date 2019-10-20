7f5672561264e32cc5af910a55a14510ce19f664
carlos <>
2019-10-20 14:39:49 -0600

    Se agregaron comandos del protocolo uci

--- src/main/java/com/wolf/carlitos/App.java
+++ src/main/java/com/wolf/carlitos/App.java
@@ -5,20 +5,34 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
        var scanner = new Scanner(System.in);
-       var juego = new Juego();
+       Juego juego = new Juego();
        
 
         while(scanner.hasNext()){
             var linea = scanner.nextLine();
-            if(linea.contains("position")){
-                var movimientos =  linea.replaceAll("position startpos moves ", "").split(" ");
-                juego = new Juego();
-                juego.EstablecerPosicion(movimientos);
-                juego.ImprimirPosicicion();
+            if(linea.contains("position startpos")){
+                if(linea.contains("moves")){
+                    var movimientos =  linea.replaceAll("position startpos moves ", "").split(" ");
+                    juego = new Juego();
+                    juego.EstablecerPosicion(movimientos);
+                    juego.ImprimirPosicicion();
+                    }
             }else if(linea.contains("go")){
                 juego.MovimientosValidos();
             }else if(linea.contains("isready"))
                 System.out.println("readyok");
+            else if(linea.contains("ucinewgame")){
+                juego = new Juego();
+                System.out.println("uciok");}
+            else if(linea.contains("uci"))
+                System.out.println("uciok");
+            else if(linea.contains("stop"))
+            {
+            System.out.println("readyok");
+            juego.MovimientosValidos();
+            }
+            
+            
         }
         scanner.close();
     }
