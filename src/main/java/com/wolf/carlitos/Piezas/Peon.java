90562e5846c7e88525de289c8fb11deb180d6332
carlos <carlos@192.168.2.6>
2019-10-20 14:35:21 -0600

    Correción en movimientos de peón

--- src/main/java/com/wolf/carlitos/Piezas/Peon.java
+++ src/main/java/com/wolf/carlitos/Piezas/Peon.java
@@ -13,100 +13,56 @@ public class Peon extends Base implements Pieza {
     public List<int[]> ObtenerMovimientos(Pieza[][] tablero, int[] posicion) {
         int filaOrigen = posicion[0];
         int columnaOrigen = posicion[1];
+        int filaDestino;
+        
         Pieza posicionActual;
         var lista = new ArrayList<int[]>();
-        if(esBlanco){
-            if(filaOrigen == 1){
-                if(tablero[2][columnaOrigen] == null && tablero[3][columnaOrigen] == null)
-                        lista.add(new int[]{filaOrigen,columnaOrigen,3,columnaOrigen});
-                if(tablero[2][columnaOrigen] == null) 
-                        lista.add(new int[]{filaOrigen,columnaOrigen,2,columnaOrigen});
-                
-            }else{
-                if(tablero[filaOrigen + 1][columnaOrigen] == null) {
-                    if(filaOrigen + 1 == 7){
-                        lista.add(new int[]{filaOrigen,columnaOrigen,filaOrigen + 1,columnaOrigen,1});
-                        lista.add(new int[]{filaOrigen,columnaOrigen,filaOrigen + 1,columnaOrigen,2});
-                        lista.add(new int[]{filaOrigen,columnaOrigen,filaOrigen + 1,columnaOrigen,3});
-                        lista.add(new int[]{filaOrigen,columnaOrigen,filaOrigen + 1,columnaOrigen,4});
-                    }else{
-                        lista.add(new int[]{filaOrigen,columnaOrigen,filaOrigen + 1,columnaOrigen});
-                    }
+        
+            //avance dos casillas
+            if(filaOrigen == (esBlanco ? 1 : 7)){
+                if(tablero[esBlanco ? 2 : 5][columnaOrigen] == null && tablero[esBlanco ? 3 : 4][columnaOrigen] == null)
+                        lista.add(new int[]{filaOrigen,columnaOrigen,esBlanco ? 3 : 4,columnaOrigen});
                 }
-
-                if(columnaOrigen > 0){
-                    posicionActual = tablero[filaOrigen + 1][columnaOrigen -1];
-                    if( posicionActual != null)
-                        if(!posicionActual.EsBlanca() && !(posicionActual instanceof Rey))
-                            if(filaOrigen + 1 == 7){
-                                lista.add(new int[]{filaOrigen,columnaOrigen,filaOrigen + 1,columnaOrigen -1,1});
-                                lista.add(new int[]{filaOrigen,columnaOrigen,filaOrigen + 1,columnaOrigen -1,2});
-                                lista.add(new int[]{filaOrigen,columnaOrigen,filaOrigen + 1,columnaOrigen -1,3});
-                                lista.add(new int[]{filaOrigen,columnaOrigen,filaOrigen + 1,columnaOrigen -1,4});
-                            }else{
-                                lista.add(new int[]{filaOrigen,columnaOrigen,filaOrigen + 1,columnaOrigen -1});
-                            }
+            //avance una casilla
+            filaDestino = filaOrigen + (esBlanco ? 1 : -1);
+            if(tablero[filaDestino][columnaOrigen] == null) {
+                if(filaDestino == 7){
+                    lista.add(new int[]{filaOrigen,columnaOrigen,filaDestino,columnaOrigen,1});
+                    lista.add(new int[]{filaOrigen,columnaOrigen,filaDestino,columnaOrigen,2});
+                    lista.add(new int[]{filaOrigen,columnaOrigen,filaDestino,columnaOrigen,3});
+                    lista.add(new int[]{filaOrigen,columnaOrigen,filaDestino,columnaOrigen,4});
+                }else{
+                    lista.add(new int[]{filaOrigen,columnaOrigen,filaDestino,columnaOrigen});
+                }
+            }
+            
+            if(columnaOrigen > 0){
+                posicionActual = tablero[filaDestino][columnaOrigen -1];
+                if( posicionActual != null)
+                    if(!posicionActual.EsBlanca() && !(posicionActual instanceof Rey))
+                        if(filaDestino == (esBlanco ? 7 : 0)){
+                            lista.add(new int[]{filaOrigen,columnaOrigen,filaDestino,columnaOrigen -1,1});
+                            lista.add(new int[]{filaOrigen,columnaOrigen,filaDestino,columnaOrigen -1,2});
+                            lista.add(new int[]{filaOrigen,columnaOrigen,filaDestino,columnaOrigen -1,3});
+                            lista.add(new int[]{filaOrigen,columnaOrigen,filaDestino,columnaOrigen -1,4});
+                        }else{
+                            lista.add(new int[]{filaOrigen,columnaOrigen,filaDestino,columnaOrigen -1});
                         }
+                 }
                 if(columnaOrigen < 7){
-                    posicionActual = tablero[filaOrigen + 1][columnaOrigen +1];
+                    posicionActual = tablero[filaDestino][columnaOrigen +1];
                     if(posicionActual!= null)
                         if(!posicionActual.EsBlanca() && !(posicionActual instanceof Rey))
-                            if(filaOrigen + 1 == 7){
-                                lista.add(new int[]{filaOrigen,columnaOrigen,filaOrigen + 1,columnaOrigen +1,1});
-                                lista.add(new int[]{filaOrigen,columnaOrigen,filaOrigen + 1,columnaOrigen +1,2});
-                                lista.add(new int[]{filaOrigen,columnaOrigen,filaOrigen + 1,columnaOrigen +1,3});
-                                lista.add(new int[]{filaOrigen,columnaOrigen,filaOrigen + 1,columnaOrigen +1,4});
+                            if(filaDestino == (esBlanco ? 7 : 0)){
+                                lista.add(new int[]{filaOrigen,columnaOrigen,filaDestino,columnaOrigen +1,1});
+                                lista.add(new int[]{filaOrigen,columnaOrigen,filaDestino,columnaOrigen +1,2});
+                                lista.add(new int[]{filaOrigen,columnaOrigen,filaDestino,columnaOrigen +1,3});
+                                lista.add(new int[]{filaOrigen,columnaOrigen,filaDestino,columnaOrigen +1,4});
                             }else{
-                                lista.add(new int[]{filaOrigen,columnaOrigen,filaOrigen + 1,columnaOrigen +1});
+                                lista.add(new int[]{filaOrigen,columnaOrigen,filaDestino,columnaOrigen +1});
                             }
                         }
-            }
-        }else{
-            if(filaOrigen == 6){
-                if(tablero[5][columnaOrigen] == null && tablero[4][columnaOrigen] == null)
-                        lista.add(new int[]{filaOrigen,columnaOrigen,4,columnaOrigen});
-                if(tablero[5][columnaOrigen] == null) 
-                        lista.add(new int[]{filaOrigen,columnaOrigen,5,columnaOrigen});
-                
-            }else{
-                if(tablero[filaOrigen -1 ][columnaOrigen] == null) 
-                    if(filaOrigen -1 == 0){
-                        lista.add(new int[]{filaOrigen,columnaOrigen,filaOrigen - 1,columnaOrigen,1});
-                        lista.add(new int[]{filaOrigen,columnaOrigen,filaOrigen - 1,columnaOrigen,2});
-                        lista.add(new int[]{filaOrigen,columnaOrigen,filaOrigen - 1,columnaOrigen,3});
-                        lista.add(new int[]{filaOrigen,columnaOrigen,filaOrigen - 1,columnaOrigen,4});
-                    }else{
-                        lista.add(new int[]{filaOrigen,columnaOrigen,filaOrigen - 1,columnaOrigen});
-                    }
-
-                    if(columnaOrigen > 0){
-                        posicionActual = tablero[filaOrigen -1][columnaOrigen -1];
-                    if(posicionActual != null)
-                        if(posicionActual.EsBlanca() && !(posicionActual instanceof Rey))
-                            if(filaOrigen - 1 == 0){
-                                lista.add(new int[]{filaOrigen,columnaOrigen,filaOrigen - 1,columnaOrigen -1,1});
-                                lista.add(new int[]{filaOrigen,columnaOrigen,filaOrigen - 1,columnaOrigen -1,2});
-                                lista.add(new int[]{filaOrigen,columnaOrigen,filaOrigen - 1,columnaOrigen -1,3});
-                                lista.add(new int[]{filaOrigen,columnaOrigen,filaOrigen - 1,columnaOrigen -1,4});
-                            }else{
-                                lista.add(new int[]{filaOrigen,columnaOrigen,filaOrigen - 1,columnaOrigen -1});
-                            }
-                        }
-                    if(columnaOrigen < 7){
-                        posicionActual = tablero[filaOrigen -1][columnaOrigen +1];
-                    if( posicionActual != null)
-                        if(posicionActual.EsBlanca() && !(posicionActual instanceof Rey))
-                            if(filaOrigen - 1 == 0){
-                                lista.add(new int[]{filaOrigen,columnaOrigen,filaOrigen - 1,columnaOrigen +1,1});
-                                lista.add(new int[]{filaOrigen,columnaOrigen,filaOrigen - 1,columnaOrigen +1,2});
-                                lista.add(new int[]{filaOrigen,columnaOrigen,filaOrigen - 1,columnaOrigen +1,3});
-                                lista.add(new int[]{filaOrigen,columnaOrigen,filaOrigen - 1,columnaOrigen +1,4});
-                            }else{
-                                lista.add(new int[]{filaOrigen,columnaOrigen,filaOrigen - 1,columnaOrigen +1});
-                            }
-                        }
-            }
-        }
+            
         System.out.print("Peon " + esBlanco + " genero: ");
         lista.forEach((pos) -> {
             System.out.print(Juego.juego.ConvertirANotacion(pos) + " ");
